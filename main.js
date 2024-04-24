ccc142237f9779f52aec68eba7a953c4c95d40c7
Kamedin <dev@dev.dev>
2024-04-24 20:18:18 +0200

    removed clientRect

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 const a = document.querySelector("a");
 const u = document.querySelector(".underline");
 
-const a_data = a.getBoundingClientRect();
-console.table(a_data);
 a.addEventListener("mouseenter", () => {
     u.style.right = "unset";
     u.style.left = "0";
